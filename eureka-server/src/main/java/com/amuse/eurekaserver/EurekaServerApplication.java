1134ff37b00390abe2076f7c7403969eeec317f2
yule <Yl3628691*>
2018-08-09 20:44:47 +0800

    创建服务注册中心

---
+++ eureka-server/src/main/java/com/amuse/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.amuse.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Auther: yule
+ * @Date: 2018/8/9 0009 20:39
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
